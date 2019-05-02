989c6ce2c8a627c2f8baa806a1359f2da4f7e4e9
Albawab <albawab94@gmail.com>
2019-05-02 17:26:06 +0200

    This is the first week of Javascript

---
+++ js1-2-8.js
@@ -0,0 +1,24 @@
+let firstNumber = 7,
+    secondNumber = 10;
+
+console.log("The value of my variable is: " + firstNumber);
+console.log(typeof(firstNumber));
+
+if(typeof(firstNumber) == typeof(secondNumber)){
+    console.log("SAME TYPE");
+}
+
+
+/* let firstNumber = 7,
+secondNumber = 10; */
+
+let firstString = ture,
+    first = [0,"hello",[1,2]];
+
+if(typeof(first) == typeof(firstString)){
+    console.log("SAME TYPE");
+}else{
+    console.log("DIFFERENT TYPE")
+}
+    
+
