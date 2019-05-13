ec47a147c038b8a5ec93bc6b5ef5dd1927835395
Albawab <32767497+Albawab@users.noreply.github.com>
2019-05-13 23:38:59 +0200

    Delete String.js

--- String.js
+++
@@ -1,9 +0,0 @@
-let myString = "hello,this,is,a,difficult,to,read,sentence";
-
-// Log the length of myString.
-console.log(myString.length);
-
-// Replace the commas
-var replace = myString.replace(/,/g , " ");
-console.log(replace);
-
